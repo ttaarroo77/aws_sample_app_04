8a1f86c15feb2ef7a65581c955817ea79fdbbb6c
tarou.nakazawa <tarou.nakazawa@gmail.com>
2023-11-29 09:24:05 +0000

    Use SSL and the Puma webserver in production-02

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,12 +20,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(params[:user])    # 実装は終わっていないことに注意!　　これと下は何が違う？？
     @user = User.new(user_params)
-    if @user.save       # 保存の成功をここで扱う。
-      flash[:success] = "Welcome to the Sample App!"  #リスト 7.29:ユーザー登録ページにフラッシュメッセージを追加する
-      redirect_to @user　#リスト 7.28:保存とリダイレクトを行う、userのcreateアクション
-      #redirect_to user_url(@user)   上と同じ
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -38,6 +36,68 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 end
+
+
+
+
+
+
+
+
+
+# app/controllers/users_controller.rb
+
+# リスト 7.5:Usersコントローラのshowアクション
+# リスト 7.6:debuggerをUsersコントローラに差し込む
+# リスト 7.7:debuggerをUsersコントローラーから取り外す
+# リスト 7.14:newアクションに@user変数を追加する
+# リスト 7.18:ユーザー登録の失敗に対応できるcreateアクション
+# リスト 7.19:createアクションでStrong Parametersを使う
+# リスト 7.28:保存とリダイレクトを行う、userのcreateアクション
+# リスト 7.29:ユーザー登録ページにフラッシュメッセージを追加する
+
+# class UsersController < ApplicationController
+
+#   def show
+#     @user = User.find(params[:id])
+#   end
+
+#   def new
+#     @user = User.new
+#   end
+
+#   def create
+#     # @user = User.new(params[:user])    # 実装は終わっていないことに注意!　　これと下は何が違う？？
+#     @user = User.new(user_params)
+#     if @user.save       # 保存の成功をここで扱う。
+#       flash[:success] = "Welcome to the Sample App!"  #リスト 7.29:ユーザー登録ページにフラッシュメッセージを追加する
+#       redirect_to @user　#リスト 7.28:保存とリダイレクトを行う、userのcreateアクション
+#       #redirect_to user_url(@user)   上と同じ
+#     else
+#       render 'new'
+#     end
+#   end
+
+#   private
+
+#     def user_params
+#       params.require(:user).permit(:name, :email, :password,
+#                                   :password_confirmation)
+#     end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
